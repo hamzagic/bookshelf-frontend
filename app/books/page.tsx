9cdf520ffffac13a57b168c6e3d7068f46eb60e9
Francis Hamzagic <francishamzagic@gmail.com>
2024-09-18 01:15:04 -0300

    started books page

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,10 +1,18 @@
-import styles from './page.module.css';
-
 import React from 'react';
+
+import Header from '@/components/header/Header';
+import BookItem from '@/components/book-item/BookItem';
+import books from '../../assets/data/books';
+import styles from './page.module.css';
 
 const BooksPage: React.FC = () => {
   return (
-    <div>Books</div>
+    <>
+    <Header pageName='My Books' />
+    <div className={styles.container}>
+      {books.map(book => <BookItem book={book} key={book.id}/>)}
+    </div>
+    </>
   );
 }
 
--- components/book-item/BookItem.tsx
+++ components/book-item/BookItem.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image';
 import styles from './BookItem.module.css';
 
-const BookItem = () => {
-  return(
+// todo: add reading percentage, add options to delete book or edit book data
+const BookItem = ({ book }: { book: { title: string; author: string; pages: number } }) => {
+  console.log('kalslfa', book);
+  return (
     <div className={styles.container}>
-      <Image src="/images/book-placeholder.png" width={50} height={50} alt="" />
-      <p className={styles.title}>Book Title</p>
-      <p className={styles.author}>Book Author</p>
+      <Image src="/images/book-placeholder.png" width={50} height={50} alt="Book cover" />
+      <p className={styles.title}>{book.title}</p>
+      <p className={styles.author}>{book.author}</p>
+      <p className={styles.pages}>{book.pages} pages</p>
+      <div>5% completed</div>
+      <div>edit</div>
+      <div>delete</div>
     </div>
   );
 }
